807b3b049b7b3df0bc41dd0a883aba4bd4e04bbf
Bal67 <bal67@duke.edu>
2024-06-19 00:44:03 -0500

    commit

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ from models.cnn_model import train_cnn
 def download_data():
     url = "https://www.kaggle.com/neha1703/movie-genre-from-its-poster/download"
     data_path = os.path.join("data", "raw")
-    os.makedirs(data_path, exist_ok=True)
+    
+    # Ensure the directory does not already exist to avoid errors
+    if not os.path.exists(data_path):
+        os.makedirs(data_path)
     
     zip_path = os.path.join(data_path, "movie_poster_dataset.zip")
     
